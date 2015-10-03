a63b8a43b0ded4a4f679667286067dea5f6de848
Blake Renton <elblayko@gmail.com>
2015-10-03 13:59:01 -0500

    Added world spacing. This is a WIP!

--- models/Player.js
+++ models/Player.js
@@ -1,71 +1,88 @@
-var Player = function(playerData) {
+var Player = function() {
+    var uid     = '';
+    var name    = '';
+    var level   = 1;
+    var posX    = 0;
+    var posY    = 0;
+    var width   = 0;
+    var height  = 0;
+    var model   = new Image();
 
-    this.name   = playerData.name   || '';
-    this.level  = playerData.level  || 1;
+    return {
+        create: function(parent, playerData) {
+            
+            // https://stackoverflow.com/questions/6248666/how-to-generate-short-uid-like-ax4j9z-in-js
+            this.uid = ('0000' + (Math.random() * Math.pow(36, 4) << 0).toString(36).slice(-4));
 
-    this.posX = playerData.position.x || 0;
-    this.posY = playerData.position.y || 0;
+            this.name   = playerData.name   || '';
+            this.level  = playerData.level  || 1;
 
-    this.model      = new Image();
-    this.model.src  = playerData.model;
+            this.posX = playerData.position.x || 0;
+            this.posY = playerData.position.y || 0;
 
-    this.width  = 64;
-    this.height = 88;
+            model.src  = playerData.model;
 
-    /**
-     *  @name           Player.draw
-     *  @description    Draws the representation of our character to the canvas.
-     *
-     */
-    this.draw = function() {
-        ctx.save();
-        ctx.drawImage(this.model, this.posX, this.posY);
-        ctx.restore();
-    };
+            this.width  = 64;
+            this.height = 88;
+        
+            parent.addEntity(this);
+        },
 
-    /**
-     *  @name           Player.setX
-     *  @params         x - The new x-coordinate on the canvas.
-     *  @description    Changes the x-coordinate of the player object on the canvas.
-     *                  Checks to make sure that the coordinate is within bounds of
-     *                  the playable area.
-     *
-     */
-    this.setX = function(x) {
-        if (x > canvas.width - this.width * 2) {
-            this.posX = canvas.width - this.width * 2;
+        /**
+         *  @name           Player.draw
+         *  @description    Draws the representation of our character to the canvas.
+         *
+         */
+        draw: function() {
+            ctx.save();
+            ctx.drawImage(model, posX, posY);
+            ctx.restore();
+        },
+
+        /**
+         *  @name           Player.setX
+         *  @params         x - The new x-coordinate on the canvas.
+         *  @description    Changes the x-coordinate of the player object on the canvas.
+         *                  Checks to make sure that the coordinate is within bounds of
+         *                  the playable area.
+         *
+         */
+        setX: function(x) {
+            if (x > canvas.width - this.width * 2) {
+                this.posX = canvas.width - this.width * 2;
+            }
+            if (x < 0) {
+                this.posX = 0;
+            }
+        },
+
+        /**
+         *  @name           Player.setY
+         *  @params         y - The new y-coordinate on the canvas.
+         *  @description    Changes the y-coordinate of the player object on the canvas.
+         *                  Checks to make sure that the coordinate is within bounds of
+         *                  the playable area.
+         *
+         */
+        setY: function(y) {
+            if (y > canvas.height - this.height) {
+                this.posY = canvas.height - this.height;
+            }
+            if (y < 0) {
+                this.posY = 0;
+            }
+        },
+
+        /**
+         *  @name           Player.setPositionOffset
+         *  @params         x - The x-coordinate offset.
+         *                  y - The y-coordinate offset.
+         *  @description    Helper function to offset the player's position.
+         *
+         */
+        setPositionOffset: function(x, y) {
+            this.setX(this.posX += x);
+            this.setY(this.posY += y);
         }
-        if (x < 0) {
-            this.posX = 0;
-        }
-    };
-
-    /**
-     *  @name           Player.setY
-     *  @params         y - The new y-coordinate on the canvas.
-     *  @description    Changes the y-coordinate of the player object on the canvas.
-     *                  Checks to make sure that the coordinate is within bounds of
-     *                  the playable area.
-     *
-     */
-    this.setY = function(y) {
-        if (y > canvas.height - this.height) {
-            this.posY = canvas.height - this.height;
-        }
-        if (y < 0) {
-            this.posY = 0;
-        }
-    };
-
-    /**
-     *  @name           Player.setPositionOffset
-     *  @params         x - The x-coordinate offset.
-     *                  y - The y-coordinate offset.
-     *  @description    Helper function to offset the player's position.
-     *
-     */
-    this.setPositionOffset = function(x, y) {
-        this.setX(this.posX += x);
-        this.setY(this.posY += y);
     };
 };
