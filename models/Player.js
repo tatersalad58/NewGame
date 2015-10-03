d1c898c600c38dd1d00087ef7f13f14bb8468258
Blake Renton <elblayko@gmail.com>
2015-10-03 01:03:11 -0500

    Added generic Link sprite placeholder. Player model constructor now takes an object.

--- models/Player.js
+++ models/Player.js
@@ -1,46 +1,45 @@
-var Player = function(name, level) {
-	this.name	= name;
-	this.level	= level;
+var Player = function(playerData) {
+    this.name   = playerData.name;
+    this.level  = playerData.level;
 
-	this.posX = 0;
-	this.posY = 0;
+    this.posX = playerData.position.x || 0;
+    this.posY = playerData.position.y || 0;
 
-	this.width = 50;
-	this.height = 50;
+    this.width = 50;
+    this.height = 50;
 
-	this.draw = function() {
-		ctx.save();
-		this.width = ctx.measureText(this.name).width * 2;
+    this.model = new Image();
+    this.model.src = playerData.model;
 
-		ctx.fillStyle = 'black';
-		ctx.fillRect(this.posX, this.posY, this.width, this.height);
+    this.draw = function() {
+        ctx.save();
+        this.width = ctx.measureText(this.name).width * 2;
 
-		ctx.fillStyle = 'white';
-		ctx.fillText(this.name, this.posX + (this.width / 4), this.posY + (this.height / 2));
-		
-		ctx.restore();
-	};
+        ctx.drawImage(this.model, this.posX, this.posY);
+        
+        ctx.restore();
+    };
 
-	this.setX = function(x) {
-		if (x > canvas.width - this.width) {
-			this.posX = canvas.width - this.width;
-		}
-		if (x < 0) {
-			this.posX = 0;
-		}
-	};
+    this.setX = function(x) {
+        if (x > canvas.width - this.width) {
+            this.posX = canvas.width - this.width;
+        }
+        if (x < 0) {
+            this.posX = 0;
+        }
+    };
 
-	this.setY = function(y) {
-		if (y > canvas.height - this.height) {
-			this.posY = canvas.height - this.height;
-		}
-		if (y < 0) {
-			this.posY = 0;
-		}
-	};
+    this.setY = function(y) {
+        if (y > canvas.height - this.height) {
+            this.posY = canvas.height - this.height;
+        }
+        if (y < 0) {
+            this.posY = 0;
+        }
+    };
 
-	this.setPositionOffset = function(x, y) {
-		this.setX(this.posX += x);
-		this.setY(this.posY += y);
-	};
+    this.setPositionOffset = function(x, y) {
+        this.setX(this.posX += x);
+        this.setY(this.posY += y);
+    };
 }
