7a3c17d6d60615975e5d4dbea9a5dd55547acb7d
Blake Renton <elblayko@gmail.com>
2015-10-04 02:38:32 -0500

    Add more comments to models/Player.js

--- models/Player.js
+++ models/Player.js
@@ -11,22 +11,28 @@ var Player = function() {
     var inventory;
 
     return {
+
+        /**
+         *  @name           Player.create
+         *  @description    Factory to create players.
+         *  @params         parent - Reference to the world the player exists in.
+         *                  playerData - Object of key-value pairs to initialize the
+         *                      player's stats.  Allowable values: model, position.x,
+         *                      position.y, name, level, wallet, health.
+         *  @return         this - The created player.
+         *
+         */
         create: function(parent, playerData) {
-
-            this.name   = playerData.name   || '';
-            this.level  = playerData.level  || 1;
-
+            model.src  = playerData.model;
             this.posX = playerData.position.x || 0;
             this.posY = playerData.position.y || 0;
-
-            model.src  = playerData.model;
-
             this.width  = 64;
             this.height = 88;
 
-            this.wallet = 0;
-            this.health = 100;
-
+            this.name   = playerData.name   || '';
+            this.level  = playerData.level  || 1;
+            this.wallet = playerData.wallet || 0;
+            this.health = playerData.health || 100;
             this.inventory = [];
         
             parent.addEntity(this);
@@ -47,9 +53,9 @@ var Player = function() {
         /**
          *  @name           Player.setX
          *  @params         x - The new x-coordinate on the canvas.
-         *  @description    Changes the x-coordinate of the player object on the canvas.
-         *                  Checks to make sure that the coordinate is within bounds of
-         *                  the playable area.
+         *  @description        Changes the x-coordinate of the player object on the canvas.
+         *                      Checks to make sure that the coordinate is within bounds of
+         *                      the playable area.
          *
          */
         setX: function(x) {
@@ -64,9 +70,9 @@ var Player = function() {
         /**
          *  @name           Player.setY
          *  @params         y - The new y-coordinate on the canvas.
-         *  @description    Changes the y-coordinate of the player object on the canvas.
-         *                  Checks to make sure that the coordinate is within bounds of
-         *                  the playable area.
+         *  @description        Changes the y-coordinate of the player object on the canvas.
+         *                      Checks to make sure that the coordinate is within bounds of
+         *                      the playable area.
          *
          */
         setY: function(y) {
@@ -90,19 +96,24 @@ var Player = function() {
             this.setY(this.posY += y);
         },
 
+        /**
+         *  @name           Player.addItem()
+         *  @params         item - Full object of an item to add to the player's inventory.
+         *  @description    Adds an item to the player's inventory.
+         *  @return         The index of the created item in the player's inventory.
+         *
+         */
         addItem: function(item) {
-            return this.inventory.push(item);
+            return this.inventory.push(item) - 1;
         },
 
-        hasItem: function(searchItem) {
-            this.inventory.forEach(function(item) {
-                if (item.name == searchItem.name) {
-                    return true;
-                }
-            });
-            return false;
-        },
-
+        /**
+         *  @name           Player.useItem()
+         *  @params         itemIndex - the index of the item in the player's inventory to use.
+         *  @description    Calls the onUse method of an object in the player's inventory.
+         *  @return         True if the item was used, false if the item could not be used.
+         *
+         */
         useItem: function(itemIndex) {
             if (typeof this.inventory[itemIndex].onUse !== 'function') {
                 return false;
